10638e3a95bd578bd8defecf19ec409f38073112
licr <1063240857@qq.com>
2024-09-07 12:24:19 +0800

    message

---
+++ include/sample_process.h
@@ -0,0 +1,21 @@
+
+
+#include "model_process.h"
+#include "utils.h"
+#include "acl/acl.h"
+
+
+
+
+class SampleProcess
+{
+public:
+    SampleProcess();
+
+    SampleProcess(int32_t  modelId);
+    ~SampleProcess();
+
+    // 1. 初始化模型
+
+    
+}
